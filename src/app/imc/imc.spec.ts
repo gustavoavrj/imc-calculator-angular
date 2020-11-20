f57b4265b4bea608dcb68c603f3e9f2c454eac67
gustavoavrj <gustavo.avrj@gmail.com>
2020-11-20 08:06:22 -0600

    add imc #2

---
+++ src/app/imc/imc.spec.ts
@@ -0,0 +1,54 @@
+import { imc } from "./imc"
+
+describe('imc unit tests', () => {
+  
+
+ 
+ 
+
+    it(' Estatura: 1.68 metros, Edad: 18, Peso: 50 kg, Sexo: M = 17.7', () => {
+        // Pattern AAA (Arrange - Act - Assert)
+
+        // Arrange
+        let result = 0.0;
+        
+        // Act
+        result = imc(1.68, 50);
+
+        // Assert
+        expect(result).toBe(17.71541950113379);
+    })
+    it('Estatura: 1.70 metros, Edad: 22, Peso: 89 kg, Sexo: H = IMC: 30.8, Status: Obeso, Peso normal: 53.5 - 72', () => {
+        // Arrange
+        let result = 0;
+        // Act
+        result = imc(1.70,89);
+        // Assert
+        expect(result).toBe(30.795847750865054);
+    })
+    it('Estatura: 1.55 metros, Edad: 21, Peso: 55 kg, Sexo: M = IMC: 22.9, Status: Normal, Peso normal: 44.4 - 59.8 ', () => {
+      // Pattern AAA (Arrange - Act - Assert)
+
+      // Arrange
+      let result = 0;
+      
+      // Act
+      result = imc(1.55,55);
+
+      // Assert
+      expect(result).toBe(22.892819979188342);
+  })
+    it('Estatura: 1.85 metros, Edad: 29, Peso: 93 kg, Sexo H = IMC: 27.2, Status: Sobre peso, Peso normal: 25 - 29.9', () => {
+    // Pattern AAA (Arrange - Act - Assert)
+
+    // Arrange
+    let result = 0;
+    
+    // Act
+    result = imc(1.85,93);
+
+    // Assert
+    expect(result).toBe(27.173119065010955);
+  })
+
+})
---
+++ src/app/imc/imc.ts
@@ -0,0 +1,5 @@
+export function imc(oper1,oper2){
+    let imc = 0.0;
+    imc = oper2 / Math.pow(oper1, 2)
+    return imc;
+}
