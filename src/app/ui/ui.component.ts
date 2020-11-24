94e7ec160478b378763ad24a17376f191b3505b2
gustavoavrj <gustavo.avrj@gmail.com>
2020-11-24 10:48:57 -0600

    update calculo  #9

--- src/app/ui/ui.component.ts
+++ src/app/ui/ui.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-
 import { imc } from '../imc/imc'
 import { mcm } from '../mcm/mcm'
 import { act } from '../ACT/act'
+import { asc } from '../asc/asc'
+
 
 
 @Component({
@@ -17,24 +18,38 @@ export class UiComponent implements OnInit {
   height;
   age;
   gender;
-  result;
+  result_imc;
+  status_imc;
+  result_mcm;
+  result_act;
+  result_asc;
   isShown: boolean = false ;
   ngOnInit(): void {
   }
   calculate() {
     this.isShown = ! this.isShown;
-    let result_imc = 0.0;
-    let result_mcm = 0.0;
-    let result_act = 0.0;
-    result_imc = imc(this.height, this.weight);
-    result_mcm = mcm(this.gender, this.weight, this.height);
-    result_act = act(this.gender,this.weight,this.height, this.age);
-    
-    console.log(result_imc);
-    console.log(result_mcm);
-    console.log(result_act);
-    this.result = result_act;
-    
+    this.result_imc = imc(this.height, this.weight);
+    this.result_mcm = mcm(this.gender, this.weight, this.height);
+    this.result_act = act(this.gender,this.weight,this.height, this.age);
+    this.result_asc = asc(this.weight, this.height)
+
+    if (this.result_imc < 16.0) {
+      this.status_imc = "Delgadez severa";
+  } else if (this.result_imc >= 16.0 && this.result_imc < 17.0) {
+      this.status_imc = "Delgadez moderada";
+  } else if (this.result_imc >= 17.0 && this.result_imc < 18.5) {
+      this.status_imc = "Delgadez leve";
+  } else if (this.result_imc >= 18.5 && this.result_imc < 24.9) {
+      this.status_imc = "Peso normal";
+  } else if (this.result_imc >= 24.9 && this.result_imc < 29.9) {
+      this.status_imc = "Pre-obeso";
+  } else if (this.result_imc >= 29.9 && this.result_imc < 34.9) {
+      this.status_imc = "Obesidad tipo I";
+  } else if (this.result_imc >= 34.9 && this.result_imc < 39.9){
+    this.status_imc = "Obesidad tipo II";
+  } else if (this.result_imc >= 40) {
+    this.status_imc = "Obesidad tipo III";
+  }
 
     
     
