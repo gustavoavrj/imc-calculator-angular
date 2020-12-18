38718bc1ed7bdba07a8de62ef02a1e526df0d11a
gustavoavrj <gustavo.avrj@gmail.com>
2020-12-18 01:39:04 -0600

    add dashboard v1 #23

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { UiComponent } from './ui/ui.component';
 import { FormsModule } from '@angular/forms';
 import { LandingboardComponent } from './landingboard/landingboard.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UiComponent,
-    LandingboardComponent
+    LandingboardComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
